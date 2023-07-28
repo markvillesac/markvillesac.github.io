5c3cc00234e874d302262f6f39716e11acf6215e
Cyrus Choi <77631273+cyruschoisy@users.noreply.github.com>
2023-07-27 23:47:35 -0400

    added password page

---
+++ av.js
@@ -0,0 +1,14 @@
+function checkPswd() {
+ var password = document.getElementById("pswd").value;
+ 
+ if (password == 'diluccio') {
+  window.location.href = 'av-files.html';
+ }
+ else if (password == "") {
+  alert ("Please enter a value.");
+ }
+ else{
+  alert("Passwords do not match.");
+ }
+}
+ 
