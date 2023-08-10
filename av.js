0c1085ed191f6a0119a316a4eb06a3295da25e82
Cyrus Choi <77631273+cyruschoisy@users.noreply.github.com>
2023-08-09 22:31:00 -0400

    file storage logic

--- av.js
+++ av.js
@@ -8,3 +8,27 @@ function checkPswd() {
   alert("Passwords do not match.");
  }
 }
+
+let items = [{cat:"DI", n:"DPDB Dual Input", fpath:"/assets/AV/DI Box/ART _ DPDB Dual/ART DPDB Dual DI Manual.pdf"},
+             {cat:"DI", n:"Behringer GI100", fpath:"/assets/AV/DI Box/Behringer _ GI100/Behringer GI-100 Single DI Manual.pdf"}
+
+]
+
+di = '';
+lighting = '';
+microphones = '';
+misc = '';
+mixers = '';
+speakers = '';
+stands = '';
+video = '';
+
+for (i = 0; i < items.length; i++) {
+ if (items[i].cat == 'DI') {
+  di += '<li><a target="_blank" class="text-dark" href="' + items[i].fpath + '">' + items[i].n + '</a></li>';
+ }
+}
+
+window.onload = function(){
+ document.getElementById ("di").innerHTML = di;
+}
